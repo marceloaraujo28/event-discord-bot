1372473c1301b8ae73a902bbd5b405e9051b801a
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-11-16 23:18:35 -0300

    feat: removed unnecessary types

--- src/bot/commands/types.ts
+++ src/bot/commands/types.ts
@@ -1,8 +1,5 @@
 import { CommandInteraction } from "discord.js";
-import { ParticipantTimesType } from "../types";
 
 export type OpenEventType = {
-  eventCounter: number;
   interaction: CommandInteraction;
-  eventStore: Record<string, Record<string, ParticipantTimesType>>;
 };
