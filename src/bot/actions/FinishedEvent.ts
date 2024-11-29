2580bdba75dfda491126b2e7dcb71f2fad2c09bd
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-11-29 19:49:59 -0300

    refactor: improving FinishedEvent

--- src/bot/actions/FinishedEvent.ts
+++ src/bot/actions/FinishedEvent.ts
@@ -1,22 +1,68 @@
-import { EmbedBuilder } from "discord.js";
+import { ChannelType, EmbedBuilder } from "discord.js";
 import { FinishedEventType } from "./types";
 
 export async function FinishedEvent({
   prisma,
   keyTitle,
   message,
-  user,
   embed,
 }: FinishedEventType) {
   const event = await prisma.event.findFirst({
     where: {
-      eventName: keyTitle, // Substituir pelo valor desejado
+      eventName: keyTitle,
+    },
+  });
+
+  const guildData = await prisma.guilds.findUnique({
+    where: {
+      guildID: message?.guild?.id,
     },
   });
 
   if (!event) {
     return console.error("Evento não existe!");
   }
+
+  const eventChannel = message.guild?.channels.cache.get(
+    event?.channelID ?? ""
+  );
+
+  if (!eventChannel || !eventChannel.isVoiceBased()) {
+    console.error(
+      "Canal de voz do evento não encontrado ou não é um canal de voz!"
+    );
+    return;
+  }
+
+  const members = eventChannel.members;
+  // Buscar o canal de espera (waitingVoiceChannelID)
+  const waitingVoiceChannel = message.guild?.channels.cache.get(
+    guildData?.waitingVoiceChannelID ?? ""
+  );
+
+  if (waitingVoiceChannel && waitingVoiceChannel.isVoiceBased()) {
+    // Transferir cada membro para a sala de espera
+    for (const [memberId, member] of members) {
+      await member.voice.setChannel(waitingVoiceChannel).catch((err) => {
+        console.error(`Erro ao mover membro ${member.user.tag}:`, err);
+      });
+    }
+  } else {
+    console.error("Canal de espera não encontrado ou não é de voz!");
+  }
+
+  const channelName = eventChannel.name;
+
+  await eventChannel?.delete();
+
+  const eventTextChannel = await message?.guild?.channels.create({
+    name: channelName,
+    type: ChannelType.GuildText,
+    reason: `Canal de texto criado para o ${channelName} finalizado!`,
+    parent: guildData?.endedCategoryID,
+  });
+
+  //atualizar embed
 
   const eventTotalTime = Date.now() - Number(event.startTime);
 
@@ -88,5 +134,7 @@ export async function FinishedEvent({
     .setDescription(embed.description)
     .setColor("DarkGrey");
 
-  await message.edit({ embeds: [updatedEmbed] });
+  await eventTextChannel?.send({ embeds: [updatedEmbed] });
+
+  await message.delete();
 }
