2cd24eee124eb061b71f844adbc744ba9d658251
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2025-04-01 20:17:04 -0300

    refactor: update libs

--- src/bot/utils/sendMessageChannel.ts
+++ src/bot/utils/sendMessageChannel.ts
@@ -8,8 +8,20 @@ export async function sendMessageChannel({ channelID, messageChannel, guild }: s
 
   const channel = guild.channels.cache.get(channelID);
 
-  if (channel?.isTextBased()) {
+  if (!channel) {
+    console.warn(`Aviso: O canal com ID ${channelID} não foi encontrado na guilda ${guild.id}.`);
+    return;
+  }
+
+  if (!channel?.isTextBased()) {
+    console.warn(`Aviso: O canal ${channel.id} não é baseado em texto.`);
+    return;
+  }
+
+  try {
     return await channel.send(messageChannel);
+  } catch (error) {
+    console.error(`Erro ao enviar mensagem no canal ${channel.id}:`, error);
   }
 
   return;
