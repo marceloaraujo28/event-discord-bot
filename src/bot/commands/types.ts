f80eb878a8aac71f3ee404c2f4b4b7741557cc57
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2025-05-17 13:00:22 -0300

    refactor: ajusted types

--- src/bot/commands/types.ts
+++ src/bot/commands/types.ts
@@ -10,12 +10,13 @@ type BaseType = {
 export type OpenEventType = {
   interaction: CommandInteraction | ButtonInteraction;
   guildData: Guilds;
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
 };
 
 type EventType = Prisma.EventGetPayload<{}>;
 
 export type AdminType = BaseType & { commandName: string };
-export type GlobalType = BaseType & { commandName: string; member: GuildMember | undefined };
+export type GlobalType = BaseType & { commandName: string; member: GuildMember | undefined; guildData: Guilds };
 export type SetupType = BaseType;
 export type SellerType = BaseType & { event: EventType };
 export type SimulateEventType = BaseType & { event: EventType; guildData: Guilds };
@@ -24,15 +25,16 @@ export type EventCommandsType = BaseType & { event: EventType; commandName: stri
 export type UpdateGuildFeeType = BaseType & { guildData: Guilds };
 export type UpdateSellerFeeType = BaseType & { guildData: Guilds };
 export type UpdateParticipationType = BaseType & { event: EventType; guildData: Guilds };
-export type GuildBalanceType = BaseType;
-export type BalancesType = BaseType;
-export type MemberBalanceType = BaseType;
-export type MyBalanceType = BaseType & { member: GuildMember | undefined };
+export type GuildBalanceType = BaseType & { guildData: Guilds };
+export type BalancesType = BaseType & { guildData: Guilds };
+export type MemberBalanceType = BaseType & { guildData: Guilds };
+export type MyBalanceType = BaseType & { member: GuildMember | undefined; guildData: Guilds };
 export type GuildDepositType = BaseType & { guildData: Guilds };
 export type WithdrawGuildType = BaseType & { guildData: Guilds };
 export type PayMemberType = BaseType & { guildData: Guilds };
 export type ConfiscateBalanceType = BaseType & { guildData: Guilds };
-export type TransferBalanceType = BaseType;
+export type TransferBalanceType = BaseType & { guildData: Guilds };
 export type RemoveBotType = BaseType & { guildData: Guilds };
 export type PriceType = BaseType;
 export type UpdateLanguageType = BaseType & { guildData: Guilds };
+export type HelpType = { interaction: CommandInteraction };
