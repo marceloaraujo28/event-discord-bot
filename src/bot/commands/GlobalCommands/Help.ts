507969fa8a7f3402f6d8e4614634d2df58b2a1ff
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2025-05-17 12:54:49 -0300

    feat: transalated help

--- src/bot/commands/GlobalCommands/Help.ts
+++ src/bot/commands/GlobalCommands/Help.ts
@@ -1,49 +1,36 @@
 import { EmbedBuilder } from "discord.js";
+import { HelpType } from "../types";
+import { useT } from "../../utils/useT";
 
-export function Help() {
+export function Help({ interaction }: HelpType) {
+  const language = interaction.locale;
+  const t = useT(language);
+
   return new EmbedBuilder()
     .setColor("#0099ff")
-    .setTitle("📜 Lista de Comandos")
-    .setDescription(
-      "Aqui estão todos os comandos disponíveis do bot, organizados para facilitar seu uso.\n\n⚠️ **Atenção:** Não exclua nenhuma sala criada pelo bot! Caso isso aconteça, será necessário reconfigurá-lo para evitar erros.\n\n"
-    )
+    .setTitle(t("help.title"))
+    .setDescription(t("help.description"))
     .addFields(
       {
-        name: "\uD83D\uDCB0 Preços Mercado\n",
-        value: "`/preco` - consulta o preço de um item no mercado - (Opcional a busca por tier e cidade).\n",
+        name: t("help.field1name"),
+        value: t("help.field1value"),
       },
       {
-        name: "\u2728 Configuração Inicial Gerenciamento de Saldo",
-        value:
-          "`/setup` - Configura automaticamente as salas e permissões do bot. **(Admin)**\n" +
-          "`/meu-saldo` - Consulta seu saldo atual.\n" +
-          "`/saldos` - Exibe o saldo de todos os membros da guild.\n" +
-          "`/saldo-membro` - Consulta o saldo de um membro específico.\n" +
-          "`/saldo-guild` - Verifica o saldo da guild.",
+        name: t("help.field2name"),
+        value: t("help.field2value"),
       },
       {
-        name: "\uD83D\uDCB3 Transações Financeiras",
-        value:
-          "`/depositar-guild` - Adiciona saldo ao caixa da guild. **(Admin)**\n" +
-          "`/sacar-guild` - Retira saldo do caixa da guild. **(Admin)**\n" +
-          "`/pagar-membro` - Envia um pagamento para um membro utilizando o saldo da guild. **(Admin)**\n" +
-          "`/confiscar-saldo` - Remove o saldo de um membro e adiciona à guild. **(Admin)**\n" +
-          "`/transferir-saldo` - Transfere saldo da sua conta para outro membro.",
+        name: t("help.field3name"),
+        value: t("help.field3value"),
       },
       {
-        name: "\uD83C\uDF1F Eventos e Participação",
-        value:
-          "`/vendedor` - Adiciona um vendedor ao evento.\n" +
-          "`/simular-evento` - Simula a divisão dos valores entre os participantes.\n" +
-          "`/atualizar-participacao` - Modifica a porcentagem de participação de um jogador.\n" +
-          "`/atualizar-taxa-vendedor` - Ajusta a taxa paga aos vendedores. **(Admin)**\n" +
-          "`/atualizar-taxa-guild` - Ajusta a taxa paga à guild. **(Admin)**\n" +
-          "`/depositar-evento` - Deposita um valor arrecadado no evento no saldo da guild.",
+        name: t("help.field4name"),
+        value: t("help.field4value"),
       },
       {
-        name: "\u2753 Ajuda",
-        value: "\n\nSuporte: \n[Discord Albion Event Bot](https://discord.gg/AjGZbc5b2s)\n",
+        name: t("help.field5name"),
+        value: t("help.field5value"),
       }
     )
-    .setFooter({ text: "Use os comandos corretamente para garantir a melhor experiência!" });
+    .setFooter({ text: t("help.footer") });
 }
